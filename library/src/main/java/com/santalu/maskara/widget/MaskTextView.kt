127d0f467db4ea87ad307ba00fc492112142a416
Luong Vo <vodailuong@gmail.com>
2020-12-18 10:34:32 +0700

    Don't need to use MaskChangedListener, use Maskara durectly instead on TextView

--- library/src/main/java/com/santalu/maskara/widget/MaskTextView.kt
+++ library/src/main/java/com/santalu/maskara/widget/MaskTextView.kt
@@ -2,7 +2,6 @@ package com.santalu.maskara.widget
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.text.TextWatcher
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 import com.santalu.maskara.*
@@ -26,16 +25,17 @@ class MaskTextView : AppCompatTextView {
         init(attrs)
     }
 
-    private var maskChangedListener: MaskChangedListener? = null
+    private var maskara: Maskara? = null
+    private var result: MaskResult? = null
 
     val masked: String
-        get() = maskChangedListener?.masked.orEmpty()
+        get() = result?.masked.orEmpty()
 
     val unMasked: String
-        get() = maskChangedListener?.unMasked.orEmpty()
+        get() = result?.unMasked.orEmpty()
 
     val isDone: Boolean
-        get() = maskChangedListener?.isDone ?: false
+        get() = result?.isDone ?: false
 
     private fun init(attrs: AttributeSet?) {
         context.obtainStyledAttributes(attrs, R.styleable.MaskEditText).apply {
@@ -46,31 +46,18 @@ class MaskTextView : AppCompatTextView {
             if (value.isNotEmpty()) {
                 val maskChar = if (character.isEmpty()) value.mostOccurred() else character.single()
                 val mask = Mask(value, maskChar, MaskStyle.valueOf(style))
-                maskChangedListener = MaskChangedListener(mask)
+                maskara = Maskara(mask)
             }
 
             recycle()
         }
     }
 
-    /**
-     * Let only one [maskChangedListener] allowed at a time
-     */
-    override fun addTextChangedListener(watcher: TextWatcher?) {
-        if (watcher is MaskChangedListener) {
-            removeTextChangedListener(maskChangedListener)
-            maskChangedListener = watcher
-        }
-        super.addTextChangedListener(watcher)
-    }
-
-    override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
-        addTextChangedListener(maskChangedListener)
-    }
-
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-        removeTextChangedListener(maskChangedListener)
+    override fun setText(text: CharSequence?, type: BufferType?) {
+        val masked = text?.let {
+            result = maskara?.apply(text, Action.INSERT)
+            result?.masked
+        } ?: text
+        super.setText(masked, type)
     }
 }
